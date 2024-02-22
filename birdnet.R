c3663e92d8c394f24b03cd296443909cb586b1ad
sergiolp <softshevelp@gmail.com>
2024-02-22 12:25:16 +0100

    audio format as variable

--- birdnet.R
+++ birdnet.R
@@ -11,6 +11,7 @@
 # 3) You need to specify all vars that appear on VARS specification section!
 # 4) The processing works for files with this format: # FORMAT: ID_Date_Hour.wav, example: (IREC-14_20230330_072002.wav)
 # 5) Switches are used to split the prog run (0 = off, 1 = on) in processing and postprocessing
+# 6) You need the following R libraries dependencies
 
 # Rlibraries dependencies!
 # library(data.table)
@@ -29,19 +30,20 @@ switch2 = 1 # postpro
 # VARS section (MANDATORY)
 #-------------------------------------------------------------------------------
 # SPECIFY ABSOLUTE PATHS!
-datadir = "/media/sergiolp/TOSHIBA EXT/GRABACIONES/Quintos/Qaudio3--IREC13/Data2" # Define the data directory with .wav files
+datadir = "/media/sergiolp/TOSHIBA EXT/GRABACIONES/Alcornocosas/IREC 2/Data2/" # Define the data directory with .wav files
 resultsdir = "/home/sergiolp/Desktop/results_test" # Don't create the folder, it will be created by birdnet!
 postprocessing_script = "/home/sergiolp/Work/IREC/progs/d1.31/IREC_birdnet/processOutputs.R" #path to the postpro script
 outputSite = "/home/sergiolp/Desktop/" #path for the final .csv by site
 
 # Other vars used by birdnet and postprocess script, info: https://github.com/kahst/BirdNET-Analyzer/tree/main/docs
-lat= 38.99416
+lat= 38.99416 
 lon= -3.92475
 site= "Puerto del Toro"
-min_conf = 0.1
+min_conf = 0.01
 threads = 4
 rtype = "csv"
 locale = "es"
+audio_format = ".wav" #.mp4/.mp3/.wav
 
 # Identify OS
 os_name <- Sys.info()["sysname"]; print(os_name)
@@ -53,8 +55,9 @@ os_name <- Sys.info()["sysname"]; print(os_name)
 if (switch1 == 1) {
   
   # List all .wav files in the working directory
-  files <- list.files(datadir, pattern = "\\.wav$", full.names = TRUE)
-  
+  files <- list.files(datadir, pattern = audio_format, full.names = TRUE)
+  files <- files[1:2]
+
   # Process each file
   # FORMAT: ID_Date_Hour.wav, example: (IREC-14_20230330_072002.wav)
   for (file in files) {
@@ -82,11 +85,11 @@ if (switch1 == 1) {
         if (os_name == "Windows") {
           docker_command <- sprintf(
             "docker run -v \"%s:/input\" -v \"%s:/output\" birdnet:latest analyze.py --i \"/input/%s\" --o \"/output/%s.csv\" --min_conf %s --threads %d --rtype %s --locale %s --lat \"%s\" --lon \"%s\" --week \"%s\"",
-            datadir, resultsdir, filename, sub("\\.wav$", "", filename), min_conf, threads, rtype, locale, lat, lon, week_num
+            datadir, resultsdir, filename, sub(audio_format, "", filename), min_conf, threads, rtype, locale, lat, lon, week_num
           )
         } else {
           # If Linux/Mac
-          docker_command <- sprintf("docker run -v '%s:/input' -v '%s:/output' birdnet analyze.py --i '/input/%s' --o '/output/%s.csv' --min_conf %s --threads %d --rtype %s --locale %s --lat '%s' --lon '%s' --week '%s'", datadir, resultsdir, filename, sub("\\.wav$", "", filename), min_conf, threads, rtype, locale, lat, lon, week_num)
+          docker_command <- sprintf("docker run -v '%s:/input' -v '%s:/output' birdnet analyze.py --i '/input/%s' --o '/output/%s.csv' --min_conf %s --threads %d --rtype %s --locale %s --lat '%s' --lon '%s' --week '%s'", datadir, resultsdir, filename, sub(audio_format, "", filename), min_conf, threads, rtype, locale, lat, lon, week_num)
         }
         
         # Run docker command
@@ -115,19 +118,16 @@ if (switch2 == 1) {
     postprocessing_script_win <- shQuote(postprocessing_script)
     resultsdir_win <- shQuote(resultsdir)
     outputSite_win <- shQuote(outputSite)
-    
+
     # Postpro Windows
     postprocessing_command <- sprintf("Rscript %s %s %s %s %s %s", postprocessing_script_win, shQuote(site), resultsdir_win, shQuote(lat), shQuote(lon), outputSite_win)
   } else {
     # Postpro Unix/Linux/MacOS
+
     postprocessing_command <- sprintf("Rscript %s '%s' '%s' '%s' '%s' '%s'", postprocessing_script, site, resultsdir, lat, lon, outputSite)
   }
   
   system(postprocessing_command, wait = TRUE)
   
 }
-
-
-
-
 
